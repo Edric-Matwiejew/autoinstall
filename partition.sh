7f3e5400223b081b9c880307458a5178cdb6225d
Edric Matwiejew <43461963+Edric-Matwiejew@users.noreply.github.com>
2019-05-27 22:35:29 +0800

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -3,3 +3,11 @@ disk_size=`(blockdev --getsize64 /dev/sda)`
 mem_total=`(cat /proc/meminfo | awk '/MemTotal/ {print $2}')`
 echo $disk_size
 echo $mem_total
+
+disk=dev/sda
+
+sfdisk $disk << EFO
+, 512M, ef
+, mem_totalB, 82
+, , 85
+EFO

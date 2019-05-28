57e301628a7926832d14b7d4e638f8d82b1b0228
Edric Matwiejew <43461963+Edric-Matwiejew@users.noreply.github.com>
2019-05-28 14:17:39 +0800

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+bytes_to_gigabytes=1073741824
+kilobytes_to_gigabytes=1048576
 
 list_disks() {
 echo
 echo "Detecting avaiable storage devices..."
 echo 
-
-sfdisk -l | awk ' 
-
-BEGIN{printf "%-4s %-10s %-5s %-5s\n", "No.", "Device", "Size", "Units"}
-
+n=1
+sfdisk -l | awk -v count=1 ' 
+BEGIN{printf "%-4s %-10s %-5s %-5s\n", "No.", "Disk", "Size", "Units"}
 /Disk \/dev/ {
 	gsub(":","",$2); 
 	gsub(",","", $4); 
-	printf "%-4s %-10s %-5s %-5s\n", NR")", $2, $3, $4
+	printf "%-4s %-10s %-5s %-5s\n", count++")", $2, $3, $4
 }'
 
 echo
@@ -43,10 +43,25 @@ do
 	fi
 done
 
-disk_size=`(blockdev --getsize64 /dev/sda)`
+disk_size=`(blockdev --getsize64 $disk)`
+
 mem_total_kb=`(cat /proc/meminfo | awk '/MemTotal/ {print $2}')`
 
-mem_total_mb=`(expr $mem_total_kb / 1000)`
+mem_total_mb=`(expr $mem_total_kb / 1024)`
+echo
+echo "The following partition table will be written to ${disk}:"
+
+boot_part=512
+echo
+printf "%-10s %-5s %-6s %-10s\n" "Device" "Size" "Units" "Type"
+printf "%-10s %-5.1f %-6s %-10s\n" "${disk}1" "${boot_part}" "MiB" "EFI Boot"
+echo $mem_total_kb $kilobytes_to_gigabytes | awk '{printf "%-10s %-5.1f %-6s %-10s\n", "'$disk'2", $1 / $2, "GiB", "Linux Swap"}'_
+echo $disk_size $mem_total_kb $boot_part $bytes_to_gigabytes | awk '{printf "%-10s %-5.1f %-6s %-10s\n", "'$disk'3", ($1 - $2*1024 - $3*1024*1024) / $4, "GiB", "Linux Extended"}' 
+echo
+
+echo "###################################################################"
+echo "WARNING: THIS WILL PERMANENTLY ERASE ALL DATA CURRENTLY ON ${disk}"
+echo "###################################################################"
 
 sfdisk --delete $disk
 
